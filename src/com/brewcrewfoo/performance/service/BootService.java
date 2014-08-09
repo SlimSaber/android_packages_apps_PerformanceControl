2f213c1e0dbfb7774b53da001dfb4d56cbe68611
maxwen <max.weninger@gmail.com>
2014-08-09 23:27:12 +0200

    PerformanceControl: rework time in stats fragment
    -support overall stats (need kernels support https://gerrit.omnirom.org/9005)
    -enable refresh/reset/restore support
    -added active time stat filter
    
    PS4
    -added share action
    -reset saved offsets on boot
    
    Change-Id: I4255457c382a724ccb5774714ce52dcab924ccbf

--- src/com/brewcrewfoo/performance/service/BootService.java
+++ src/com/brewcrewfoo/performance/service/BootService.java
@@ -70,6 +70,11 @@ public class BootService extends Service implements Constants {
         protected Void doInBackground(Void... args) {
 
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(c);
+
+            // clear saved offsets - they make no sense after a reboot
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(PREF_OFFSETS, "").commit();
+
             final StringBuilder sb = new StringBuilder();
             final String FASTCHARGE_PATH = Helpers.fastcharge_path();
             final String BLN_PATH = Helpers.bln_path();
